21a9b5060352d8db29755abe8452362d9f175907
Zai <tusharmistry444@gmail.com>
2022-02-03 16:44:08 -0700

    since it wasn't working i'm going back to the old version

--- commands/crash.js
+++ commands/crash.js
@@ -1,48 +1,17 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
 const wait = require('util').promisify(setTimeout);
-const { MessageActionRow, MessageButton} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('crash')
         .setDescription('Crash the bot... maybe'),
     async execute(interaction) {
-
-        const yesNo = new MessageActionRow()
-        .addComponents(
-            new MessageButton()
-                .setCustomId('yes')
-                .setLabel('Yes')
-                .setStyle('SUCCESS'),
-            new MessageButton()
-                .setCustomId('no')
-                .setLabel('No')
-                .setStyle('DANGER')
-        );
-
-        const message = await interaction.reply({content: 'Are you sure?\nYou have 10 seconds.', components: [yesNo], fetchReply: true});
-        await wait(10000);
-        await interaction.editReply({content: 'Timed out', components: []});
-        const collector = message.createMessageComponentCollector({ componentType: 'BUTTON', time: 10000});
-            collector.on('collect', i => {
-                if (i.user.id === interaction.user.id) {
-                    switch (i.customId) {
-                        case 'yes':
-                            i.update({content: 'Crashing the bot in 3...', components: []})
-                            wait(1000)
-                            i.update('Crashing the bot in 2...')
-                            wait(1000)
-                            i.update('Crashing the bot in 1...')
-                            wait(1000)
-                            i.update(`${interaction.user.username} is not in the sudoers file. This incident will be reported.`)
-                            break;
-                        case 'no':
-                            i.update({content: ':D', components: []})
-                        
-                    }
-                } else {
-                    i.reply({content: 'no', components: []})
-                }
-            })
+        await interaction.reply(`Crashing the bot in 3...`);
+        await wait(1000);
+        await interaction.editReply(`Crashing the bot in 2...`);
+        await wait(1000);
+        await interaction.editReply(`Crashing the bot in 1...`);
+        await wait(1000);
+        await interaction.editReply(`${interaction.user.username} is not in the sudoers file. This incident will be reported.`);
     },
 };
