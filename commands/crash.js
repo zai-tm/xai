7e78d40f5476570ce9dcd3ae8eedacfcaa1c6490
Zai <tusharmistry444@gmail.com>
2022-02-03 08:21:33 -0700

    s

--- commands/crash.js
+++ commands/crash.js
@@ -1,11 +1,30 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
 const wait = require('util').promisify(setTimeout);
+const { MessageActionRow, MessageButton} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('crash')
         .setDescription('Crash the bot... maybe'),
     async execute(interaction) {
+
+        const yesNo = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setCustomId('yes')
+                .setLabel('Yes')
+                .setStyle('SUCCESS'),
+            new MessageButton()
+                .setCustomId('no')
+                .setLabel('No')
+                .setStyle('DANGER')
+        );
+
+        await interaction.reply('Are you sure?\nYou have 10 seconds.', {components: [yesNo], fetchReply: true});
+        await wait(10000);
+        await interaction.reply('Timed out', {});
+        const collector = interaction.message.createMessageComponentCollector({ componentType: 'BUTTON', time: 10000});
+
         await interaction.reply(`Crashing the bot in 3...`);
         await wait(1000);
         await interaction.editReply(`Crashing the bot in 2...`);
