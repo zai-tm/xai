389101ef78e19004033ed3482cfba1d1d65b1dd8
Zai <74737630+zai-tm@users.noreply.github.com>
2022-02-07 12:25:17 -0700

    help 2

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const {MessageEmbed} = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('Help me I am trapped in a discord bot and now Luihum is calling me an AI because I\'m trapped in a discord bot'),
+        .setDescription('Help me I am trapped in a discord bot and now Luihum is calling me an AI'),
     async execute(interaction) {
         await interaction.reply('This Help is to help you to use Help. Use help when you need help. Hope this helps!');
     },
