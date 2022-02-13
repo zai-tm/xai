e76a43b2b7b8c09d45aa020a47547c448ce62f78
Zai <tusharmistry444@gmail.com>
2022-02-13 10:21:24 -0700

    remove unused packages

--- commands/credit.js
+++ commands/credit.js
@@ -6,7 +6,7 @@ module.exports = {
         .setName('credit')
         .setDescription('What is your Progress Credit™?'),
     async execute(interaction) {
-        const number = Math.floor(Math.random() * 8)
+        const number = Math.floor(Math.random() * 10)
         const credit = 0
         const image = `https://zai-tm.github.io/credit/${number}.png`
 
